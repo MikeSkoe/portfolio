a697d229d68be3490b9a1c9d83daf5b1c3da6354
Mike Skoe <rapidmajk@yandex.ru>
2024-03-09 23:59:38 +0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+class MyAvatar extends HTMLElement {
+    static template = function () {
+        const tmpl = document.createElement("template");
+        tmpl.innerHTML = `
+            <style>
+            </style>
+
+            <div></div>
+        `;
+        return tmpl;
+    }();
+
+    connectedCallback() {
+        this.attachShadow({ mode: 'open' }).appendChild(
+            MyAvatar.template.content.cloneNode(true)
+        );
+    }
+}
+
+customElements.define("my-avatar", MyAvatar);
